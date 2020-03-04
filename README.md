77f9c119a7f0fa81e905c824517cd7d116b1fc4b
Ryan Gniadek <ryan@gniadek.net>
2020-03-04 01:01:10 -0500

    Dockerfile updated with POST function. README has sample POST curl
    Need to add to list and implement get and remove

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Babble App  
 
 Babble is going to be a multi-container application. It is being implemented by a Ryan Gniadek and Ben Bernstein for the Docker Containerization special interest course taught at Virginia Tech.
+
+
+Sample POST request via CURL 
+`curl -X POST -d '{ "author": { "email": "user@example.com", "name": "string"}, "message": "Hello there"}' localhost/blabs --header "Content-Type:application/json"`
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request
+import time
 app = Flask(__name__)
+
+responses = []
 
 @app.route('/blabs')
 def get_blabs():
@@ -7,3 +10,17 @@ def get_blabs():
 
         #right now just return the timestamp until the rest is implemented
         return created_since
+
+@app.route('/blabs', methods = ['POST'])
+def add_blab():
+        author = request.get_json().get('author')
+        message = request.get_json().get('message')
+        
+        #need to not hardcode in the post id
+        response = {
+                'id': str(1), 
+                'postTime':int(time.time()),
+                'author': author,
+                'message': message }
+        responses.append(response)
+        return jsonify(response)

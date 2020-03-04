f8e0a0c15a7c3ae70945e86b3ba3998dcf145d4c
Ryan Gniadek <ryan@gniadek.net>
2020-03-04 00:28:01 -0500

    fixing .gitignore

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-greetings = ["hello", "hey"]
+@app.route('/blabs')
+def get_blabs():
+        created_since = request.query_string
 
-@app.route('/')
-def get_all_greetings():
-        return jsonify(greetings)
-
-@app.route( '/greetings' , methods = [ 'POST'])
-def add_greeting():
-        content = request.json
-        newGreeting = "Hello %s" % content[ 'name']
-        greetings.append(newGreeting)
-        return jsonify({ "greeting" : newGreeting })
-
-app.run(host="0.0.0.0")
-
-#docker run -p 5000:5000 neberizer/babble
+        #right now just return the timestamp until the rest is implemented
+        return created_since

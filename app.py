edf4bd3e7471fe42bba97c1190f7286da6702a70
Ryan Gniadek <ryan@gniadek.net>
2020-03-04 01:02:37 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ responses = []
 def get_blabs():
         created_since = request.query_string
 
-        #right now just return the timestamp until the rest is implemented
+        
         return created_since
 
 @app.route('/blabs', methods = ['POST'])

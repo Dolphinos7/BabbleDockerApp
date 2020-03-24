a3bf0828a0445e17890f128b607132f1df1d79f4
Ryan Gniadek <ryan@gniadek.net>
2020-03-24 14:36:33 -0400

    Finished Get Blabs

--- app.py
+++ app.py
@@ -3,28 +3,29 @@ import time
 app = Flask(__name__)
 
 responses = []
+next_id = 1
 
 @app.route('/blabs', methods = ['GET'])
 def get_blabs():
-        created_since = request.args
-        return jsonify(responses)
-
-
-        #created_since = request.query_string
-        #This was just for testing queries, not at all what spec requires us to do
-        
-        #return created_since
+        args = request.args
+        created_since = args.get("createdSince")
+        toReturn = []
+        for response in responses:
+                if response.get("postTime")>=int(created_since):
+                        toReturn.append(response)
+        return jsonify(toReturn)
 
 @app.route('/blabs', methods = ['POST'])
 def add_blab():
+        global next_id
         author = request.get_json().get('author')
         message = request.get_json().get('message')
         
-        #need to not hardcode in the post id
         response = {
-                'id': str(1), 
+                'id': str(next_id), 
                 'postTime':int(time.time()),
                 'author': author,
                 'message': message }
         responses.append(response)
+        next_id+=1
         return jsonify(response)
